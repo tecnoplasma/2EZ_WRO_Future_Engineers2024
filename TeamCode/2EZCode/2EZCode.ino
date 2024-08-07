964d9ce71b871341239d4da676ba43c925da0ce3
tecnoplasma <156679431+tecnoplasma@users.noreply.github.com>
2024-08-07 23:20:59 +0530

    Bug fixes for errors w read

--- TeamCode/2EZCode/2EZCode.ino
+++ TeamCode/2EZCode/2EZCode.ino
@@ -23,8 +23,8 @@ const int width=2;
 const int height=3;
 const int age=4;
 
-int[] greenStuff=new int[5];
-int[] redStuff=new int[5];
+int greenStuff[5];
+int redStuff[5];
 bool twoReads;
 
 
@@ -69,32 +69,32 @@ static void read()
   if (pixy.ccc.numBlocks)
   {
     if ((pixy.ccc.blocks[0].m_signature)==greenSig){
-      getBulkValue(greenStuff,0)
-    };
+      getBulkValue(greenStuff,0);
+    }
     if ((pixy.ccc.blocks[0].m_signature)==redSig){
-      getBulkValue(redStuff,0)
-    };
+      getBulkValue(redStuff,0);
+    }
 
     if (pixy.ccc.numBlocks>1) {
       twoReads=true;
       if ((pixy.ccc.blocks[1].m_signature)==greenSig){
-        getBulkValue(greenStuff,1)
-      };
+        getBulkValue(greenStuff,1);
+      }
       if ((pixy.ccc.blocks[1].m_signature)==redSig){
-        getBulkValue(redStuff,1)
-      };
+        getBulkValue(redStuff,1);
+      }
     }  
     else{
       twoReads=false;
-    } 
+    }
   }
 }
 
-static void getBulkValue(int[] sigArr, int index) 
+static void getBulkValue(int sigArr[], int index) 
 {
-    sigArr[x]=pixy.ccc.blocks[index].m_x);
-    sigArr[y]=pixy.ccc.blocks[index].m_y);
-    sigArr[width]=pixy.ccc.blocks[index].m_width);
-    sigArr[height]=pixy.ccc.blocks[index].m_height);
-    sigArr[age]=pixy.ccc.blocks[index].m_age);
+    sigArr[x]=pixy.ccc.blocks[index].m_x;
+    sigArr[y]=pixy.ccc.blocks[index].m_y;
+    sigArr[width]=pixy.ccc.blocks[index].m_width;
+    sigArr[height]=pixy.ccc.blocks[index].m_height;
+    sigArr[age]=pixy.ccc.blocks[index].m_age;
 }
